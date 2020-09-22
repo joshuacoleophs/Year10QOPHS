c3237fa43892159dddcc994e537f1dfa0078be7e
joshuacoleophs <71695876+joshuacoleophs@users.noreply.github.com>
2020-09-22 10:08:50 +0100

    Add files via upload

---
+++ string slicing challenge 1.py
@@ -0,0 +1,8 @@
+#Ask a user to enter their first name as lower case
+#Ask a user to enter their last name as lower case
+#Display their whole name including the space
+
+f_name = str(input("Enter your first name >>> ")).lower()
+l_name = str(input("Enter your last name >>> ")).lower()
+
+print (f_name + " " + l_name)
