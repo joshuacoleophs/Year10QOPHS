ed702490731e12687fbdce20b2006980b17a3816
joshuacoleophs <71695876+joshuacoleophs@users.noreply.github.com>
2020-09-22 10:10:15 +0100

    Add files via upload

---
+++ string slicing challenge 4.py
@@ -0,0 +1,9 @@
+#Ask a user to enter the first line of a nursery rhyme
+#Store the length of their input in a variable
+#Display the string “This has {the number of letters as above} letters in it”
+#Ask a user to enter a starting number (as an integer)
+#Ask a user to enter an ending number (as an integer)
+#Use the start/end number and store just that section in a variable
+#Display the section stored in the variable above
+
+first_line = str(input("Enter the first line of a nusery rhyme>>> "))
---
+++ string slicing challenge 5.py
@@ -0,0 +1,4 @@
+#Ask a user to enter a word/any word
+#In a variable, convert the word to uppercase
+#Display the (newly converted) uppercase word
+
